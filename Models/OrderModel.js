95f6094d1c416c00ac4f9ab5a9743ad34cdcc88c
meghanaatmakuri <meghanaatmakuri7@gmail.com>
2024-06-23 04:24:35 +0530

    adding updated server files

--- Models/OrderModel.js
+++ Models/OrderModel.js
@@ -1,13 +1,41 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-  order_id: String,
-  name: String,
-  email: String,
-  phone: String,
-  amount: Number,
-  payment_id: String,
-  created_at: { type: Date, default: Date.now }
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    payment_id: {
+        type: String,
+        required: true
+    },
+    order_id: {
+        type: String,
+        required: true,
+        unique: true // Ensure order_id is unique
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    shipmentStatus:{
+      type:String,
+      default:'packed'
+    }
 });
 
-module.exports = mongoose.model("ordercfg", orderSchema);
+const Order = mongoose.model('ordercfg', orderSchema);
+
+module.exports = Order;
