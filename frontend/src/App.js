40e5f0e49fabc1fb775227c86a449e918bfbf4bb
meghanaatmakuri <meghanaatmakuri7@gmail.com>
2024-06-23 02:29:06 +0530

    adding frontend files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import {Routes,Route} from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Products from './components/Products';
 import Categories from './components/Categories';
+import Cart from './components/Cart';
+import PaymentPage from './components/Payment';
 function App() {
   return (
     <div className="App">
@@ -12,6 +14,8 @@ function App() {
         <Route path='/admin' element={<Sidebar/>} />
         <Route path='/products' element={<Products/>}/>
         <Route path='/category' element={<Categories/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/payment' element={<PaymentPage/>}/>
       </Routes>
     </div>
   );
