887c9b8c0a21901e549feefcacceb549580c8a74
meghanaatmakuri <meghanaatmakuri7@gmail.com>
2024-06-23 04:25:13 +0530

    adding all frontend files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ import Products from './components/Products';
 import Categories from './components/Categories';
 import Cart from './components/Cart';
 import PaymentPage from './components/Payment';
+import Orders from './components/Orders'
+import OrderTracking from './components/OrderTracking';
 function App() {
   return (
     <div className="App">
@@ -16,6 +18,8 @@ function App() {
         <Route path='/category' element={<Categories/>}/>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/payment' element={<PaymentPage/>}/>
+        <Route path="/orders" element={<Orders/>}/>
+        <Route path="/tracking" element={<OrderTracking/>}/>
       </Routes>
     </div>
   );
